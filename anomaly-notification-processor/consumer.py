d169d5ad9c14d870dd7597f7aa5947361fe08d59
gabogth <gabogth@gmail.com>
2025-06-19 18:55:07 -0500

    Improve Kafka consumer and fix compose service name (#2)

--- anomaly-notification-processor/consumer.py
+++ anomaly-notification-processor/consumer.py
@@ -1,23 +1,39 @@
 import os
+import time
+from kafka import KafkaConsumer, errors
 from kafka import KafkaConsumer
 
 
 def main():
     bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
     topic = os.getenv("TOPIC", "postgres.public.hfj_res_ver")
-    consumer = KafkaConsumer(
-        topic,
-        bootstrap_servers=bootstrap_servers.split(","),
-        auto_offset_reset="earliest",
-        enable_auto_commit=True,
-        group_id="anomaly-notification-consumer",
-    )
+    consumer = None
+    while consumer is None:
+        try:
+            consumer = KafkaConsumer(
+                topic,
+                bootstrap_servers=bootstrap_servers.split(","),
+                auto_offset_reset="earliest",
+                enable_auto_commit=True,
+                group_id="anomaly-notification-consumer",
+            )
+        except errors.NoBrokersAvailable:
+            print(
+                f"Kafka brokers at {bootstrap_servers} not available, retrying..."
+            )
+            time.sleep(5)
+
     print(f"Consuming topic '{topic}' from {bootstrap_servers}")
-    for msg in consumer:
-        value = msg.value
-        if isinstance(value, bytes):
-            value = value.decode("utf-8", "ignore")
-        print(f"{msg.topic}:{msg.partition}:{msg.offset}: {value}")
+    try:
+        for msg in consumer:
+            value = msg.value
+            if isinstance(value, bytes):
+                value = value.decode("utf-8", "ignore")
+            print(f"{msg.topic}:{msg.partition}:{msg.offset}: {value}")
+    except KeyboardInterrupt:
+        print("Stopping consumer")
+    finally:
+        consumer.close()
 
 
 if __name__ == "__main__":
